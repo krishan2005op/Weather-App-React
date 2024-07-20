0d931495130e98d0b420e59a3f45181b6c26404b
krishan2005op <krishanmalhotra2005@gmail.com>
2024-07-20 18:52:38 +0530

    updated

--- weather-app-react/src/Weather-app.jsx
+++ weather-app-react/src/Weather-app.jsx
@@ -7,10 +7,17 @@ function WeatherApp(){
     const [weather,setWeather] = useState(null)
     const [error,setError] = useState(null)
     const apiKey = 'd612e60d26e26f50dfc269888436c0df';  // Replace with your API key
-    const city = 'london';  
+    const [city,setCity] = useState(""); 
+    
+
 
     useEffect(()=>{
+
         const fetchWeather = async () => {
+            if (!city) {
+                setWeather(null); // Reset weather data if input is empty
+                return;
+            }
             try {
                 const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
                 setWeather(response.data);
@@ -19,17 +26,25 @@ function WeatherApp(){
             }
         };
         fetchWeather();
-    },[])
+    },[city])
 
-    if (error) return <div>Error: {"invalid API key"}</div>;
-    if (!weather) return <div>Loading...</div>;
+    function handleCity(event){
+        setCity(event.target.value)
 
+    }
+
+    
+
+    
+    
     return(
     <>
         <div>
-            <h1>Weather in {weather.name}</h1>
-            <p>Temperature: {Math.round(weather.main.temp - 273.15)}°C</p>
-            <p>Condition: {weather.weather[0].description}</p>
+            <input type='text' value={city} onChange={handleCity} placeholder='Enter city...'/>
+            
+            <h1>Weather in {city || 'N/A'}</h1>
+            <p>Temperature: {weather ? `${Math.round(weather.main.temp - 273.15)}°C` : "null"}</p>
+            <p>Condition: {weather ? weather.weather[0].description : "null"}</p>
         </div>
 
     </>
